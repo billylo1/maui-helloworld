4ab956c620560c2defd345268741a19870a6bdd8
Billy Lo <billylo1@gmail.com>
2025-03-05 20:13:10 -0500

    testcase

---
+++ HelloWorld/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+namespace HelloWorld;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+		WelcomeLabel.Text = CultureInfo.CurrentUICulture.DisplayName;
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+
+	}
+}
+
